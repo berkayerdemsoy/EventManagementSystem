b8356a6f031f6542c67777a049e1df697373279b
berkai <berkayerdemsoy@gmail.com>
2026-03-28 21:53:09 +0300

    add validation annotations to UserCreateDto; include email format and size constraints for username, password, first name, and last name

--- UserService/user-service-client/src/main/java/com/example/user_service_client/dto/UserCreateDto.java
+++ UserService/user-service-client/src/main/java/com/example/user_service_client/dto/UserCreateDto.java
@@ -1,22 +1,31 @@
 package com.example.user_service_client.dto;
 
 import com.example.user_service_client.enums.Roles;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.NumberFormat;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserCreateDto {
+    @Size(min = 1, max = 50 , message = "Username must be between 1 and 50 characters")
     private String username;
+    @Size(min = 5, max = 16 , message = "Password must be between 5 and 16 characters")
     private String password;
+    @Email
     private String email;
     private Roles role;
 
     // UserProfile alanları (flat)
+    @Size(min = 1, max = 50 , message = "First name must be between 1 and 50 characters")
     private String firstName;
+    @Size(min = 1, max = 50 , message = "Last name must be between 1 and 50 characters")
     private String lastName;
+    @NumberFormat
     private String phoneNumber;
 }
 
