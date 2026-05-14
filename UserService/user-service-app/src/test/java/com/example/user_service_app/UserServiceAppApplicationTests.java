b2adcc1dbde8302fdcbbbebe6f09fabef4468d5f
berkai <berkayerdemsoy@gmail.com>
2026-05-14 19:34:41 +0300

    add test profiles and H2 database configuration for integration testing

--- UserService/user-service-app/src/test/java/com/example/user_service_app/UserServiceAppApplicationTests.java
+++ UserService/user-service-app/src/test/java/com/example/user_service_app/UserServiceAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.user_service_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceAppApplicationTests {
 
 	@Test
--- event-service/event-service-app/src/test/java/com/example/event_service_app/EventServiceAppApplicationTests.java
+++ event-service/event-service-app/src/test/java/com/example/event_service_app/EventServiceAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.event_service_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventServiceAppApplicationTests {
 
 	@Test
